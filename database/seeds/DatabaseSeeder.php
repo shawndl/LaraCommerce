305051927249a49e2d4b84859dbf354f8ad05bdc
Shawn Legge <shawnswebsites@gmail.com>
2018-10-19 19:45:35 +1100

    users added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
         $this->call(RoleTableSeeder::class);
         $this->call(CategoryTableSeeder::class);
         //$this->call(SeedImageTable::class);
